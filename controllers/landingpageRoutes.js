597b53a8d4ddcea69fcacb3af0454bb14094eaca
ike imbanu <imbanu1@gmail.com>
2024-04-25 09:37:54 -0300

    changes made

--- controllers/landingpageRoutes.js
+++ controllers/landingpageRoutes.js
@@ -1,22 +1,147 @@
-const router = require('express').Router();
-const { Comment, BlogPost, User } = require('../models');
-const authenticateSession = require('../utils/auth');
+const router = require("express").Router();
+const { BlogPost, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
-    try {
-        const blogPostData = await BlogPost.findAll({
-            include: [{ model: User, attributes: ['username'] }]
-        });
+router.get("/", async (req, res) => {
+  try {
+    const blogPostData = await BlogPost.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+        {
+          model: Comment,
+          attributes: ["comment_body"],
+        },
+      ],
+    });
 
-        const blogPosts = blogPostData.map((post) => post.get({ plain: true }));
+    const blogPosts = blogPostData.map((blogPost) =>
+      blogPost.get({ plain: true })
+    );
 
-        res.render('homepage', {
-            blogPosts,
-            loggedIn: req.session.loggedIn,
-        });
-    } catch (err) {
-        res.status(500).json(err);
+    res.render("landingpage", {
+      blogPosts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/blogPost/:id", withAuth, async (req, res) => {
+  try {
+    const blogPostData = await BlogPost.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    const blogPost = blogPostData.get({ plain: true });
+
+    res.render("blogPost", {
+      ...blogPost,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+    res.redirect("/login");
+  }
+});
+
+router.get("/dashboard", withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [
+        {
+          model: BlogPost,
+          include: [User],
+        },
+        {
+          model: Comment,
+        },
+      ],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("dashboard", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/create", async (req, res) => {
+  try {
+    if (req.session.logged_in) {
+      res.render("create", {
+        logged_in: req.session.logged_in,
+        userId: req.session.user_id,
+      });
+      return;
+    } else {
+      res.redirect("/login");
     }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/create/:id", async (req, res) => {
+  try {
+    const blogPostData = await BlogPost.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    const blogPost = blogPostData.get({ plain: true });
+
+    if (req.session.logged_in) {
+      res.render("edit", {
+        ...blogPost,
+        logged_in: req.session.logged_in,
+        userId: req.session.user_id,
+      });
+      return;
+    } else {
+      res.redirect("/login");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.all("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
+  }
+
+  res.render("login");
 });
 
 module.exports = router;
+;
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,15 @@
 -- Drop and create database
 DROP DATABASE IF EXISTS tech_blog_db;
 CREATE DATABASE tech_blog_db;
+
+USE tech_blog_db;
+
+-- Create user table if not exists
+CREATE TABLE IF NOT EXISTS `user` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+-- Add username column to user table
+ALTER TABLE `user` ADD COLUMN `username` VARCHAR(255) NOT NULL;
